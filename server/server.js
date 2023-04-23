b91bb7f7355ed0d4e2a86945aefddfb93c6c6990
BinoB <binob831@gmail.com>
2023-04-23 18:08:57 +0530

    name change

--- server/server.js
+++ server/server.js
@@ -23,9 +23,9 @@ __dirname = path.resolve();
 
 // render deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/client/public")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "public", "index.html"));
   });
 }
 
